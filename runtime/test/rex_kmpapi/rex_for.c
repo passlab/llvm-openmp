3eb946313ed0e068c729a2a53f7ab04da24467bc
Yonghong Yan <yanyh15@gmail.com>
2017-11-29 13:40:01 -0500

    REX_KMPAPI: add the functional rex_for.c test program

--- runtime/test/rex_kmpapi/rex_for.c
+++ runtime/test/rex_kmpapi/rex_for.c
@@ -1,13 +1,15 @@
-#include<stdio.h>
-#include<omp.h>
+#include <stdio.h>
+#include <omp.h>
+#include "rex_kmp.h"
 
 typedef struct for_body_arg_t {
 	int * A;
 	int * B;
 } for_body_arg_t;
 
-void for_body(int i, for_body_arg_t* arg) {
-	arg->A[i] = arg->A[i] + 3.14 * arg->B[i];
+void for_body(int i, void * varg) {
+    for_body_arg_t* arg = (for_body_arg_t*)varg;
+    arg->A[i] = arg->A[i] + 3.14 * arg->B[i];
 }
 
 void parallel_func(int *global_id, int num_args, int *A, int *B, int N) {
@@ -16,7 +18,7 @@ void parallel_func(int *global_id, int num_args, int *A, int *B, int N) {
    arg.A = A;
    arg.B = B;
 	
-   rex_for(0, N, 1, REX_SCHED_STATIC, -1, for_body, &arg);
+   rex_for(0, N, 1, -1, for_body, &arg);
 	
 }
 
@@ -36,7 +38,7 @@ int main(int argc, char * argv[])
 	A[i] = A[i] + 3.14 * B[i];
     }
 */
-    rex_parallel(..., parallel_func, 3, A, B, N);
+    rex_parallel(4, (rex_pfunc_t)parallel_func, 3, A, B, N);
 	
     return 0;
 }
